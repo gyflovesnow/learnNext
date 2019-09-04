ed6f60f02fef7c3a23c23119fef3fb11a7b3e7e8
guyunfei <741377056@qq.com>
2019-09-04 15:52:55 +0800

    first commit

---
+++ server/models/User.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const mongoSchema = new Schema({
+  slug: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  createdAt: {
+    type: Date,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  isAdmin: {
+    type: Boolean,
+    default: false,
+  },
+  displayName: String,
+  avatarUrl: String,
+});
+
+const User = mongoose.model('User', mongoSchema);
+
+module.exports = User;
